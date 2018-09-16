a9951c5c483cac7ef2a672da616af59e5a2dab0c
algor <pektuktuk@hotmail.com>
2018-09-17 01:22:19 +0200

    Archivos de recursos y cambios en la gestión de cuentas.

--- TukTukNimeMVC/TukTukNime/TukTukNime/App_Start/IdentityConfig.cs
+++ TukTukNimeMVC/TukTukNime/TukTukNime/App_Start/IdentityConfig.cs
@@ -1,10 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Data.Entity;
-using System.Linq;
-using System.Security.Claims;
+﻿using System.Security.Claims;
 using System.Threading.Tasks;
-using System.Web;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
 using Microsoft.AspNet.Identity.Owin;
@@ -61,9 +56,8 @@ namespace TukTukNime
             };
 
             // Configurar valores predeterminados para bloqueo de usuario
-            manager.UserLockoutEnabledByDefault = true;
-            manager.DefaultAccountLockoutTimeSpan = TimeSpan.FromMinutes(5);
-            manager.MaxFailedAccessAttemptsBeforeLockout = 5;
+            // No bloquear el usuario
+            manager.UserLockoutEnabledByDefault = false;
 
             // Registre proveedores de autenticación en dos fases. Esta aplicación usa los pasos Teléfono y Correo electrónico para recibir un código para comprobar el usuario
             // Puede escribir su propio proveedor y conectarlo aquí.
