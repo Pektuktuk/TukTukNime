a0e9b69356a0e6403e5146c2ed8bda9a0593fc65
algor <pektuktuk@hotmail.com>
2018-09-21 18:12:25 +0200

    Creado proyecto para hacer página de Kantai collection

---
+++ TukTukNimeMVC/TukTukNime/KancollTuk/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="KancollTuk.MvcApplication" Language="C#" %>
---
+++ TukTukNimeMVC/TukTukNime/KancollTuk/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(KancollTuk.Startup))]
+namespace KancollTuk
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
